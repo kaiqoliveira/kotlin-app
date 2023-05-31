1e0f53f9476ad075f805efb4bd0a4c7d3fd07bdc
Kaique Mello de Oliveira <kaiquemello2008@hotmail.com>
2023-05-31 05:09:04 -0300

    third

--- app/src/main/java/br/com/fiap/gsk/TelaPrincipal.kt
+++ app/src/main/java/br/com/fiap/gsk/TelaPrincipal.kt
@@ -2,14 +2,9 @@ package br.com.fiap.gsk
 
 import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import br.com.fiap.gsk.databinding.ActivityTelaPrincipalBinding
+
 
 class TelaPrincipal : AppCompatActivity() {
 
